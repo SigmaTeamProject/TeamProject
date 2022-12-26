5967f32951994fa99ced9ce7ddef1e613218d864
Anatoly <algash04@gmail.com>
2022-12-26 21:49:30 +0200

    Solved conflict

--- Application/Commands/Auth/Login/IdentityCommandHandler.cs
+++ Application/Commands/Auth/Login/IdentityCommandHandler.cs
@@ -29,7 +29,7 @@ namespace Application.Commands.Auth.Login
         public async Task<(CustomerModel, string)> Handle(LoginCommand request, CancellationToken cancellationToken)
         {
             var customer = await _repository.FirstOrDefault(customer =>
-                customer.Login == request.Login &&
+                customer.Login == request.Login && 
                 BCrypt.Net.BCrypt.Verify(customer.Password, request.Password));
 
             var claims = new List<Claim>
