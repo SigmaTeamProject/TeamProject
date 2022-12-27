6d5f88f8fe268fc775097eab2833e4929b9c7af1
Lyub4i <87826275+Lyub4i@users.noreply.github.com>
2022-12-27 17:15:46 +0200

    Chahge PaymentConfig model

--- Data/PaymentConfig.cs
+++ Data/PaymentConfig.cs
@@ -3,7 +3,5 @@ namespace Data;
 public class PaymentConfig
 {
     public int Id { get; set; }
-    public int CustomerId { get; set; }
-    public Customer? Customer { get; set; }
     public string Type { get; set; }
 }
