d16697af241de64a4f1d79923c0a90bb1b1de334
Nelik <112435537+nelicic@users.noreply.github.com>
2022-12-27 15:59:40 +0200

    added logger

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,17 +1,25 @@
+using Application.Services.Implementation;
+using Application.Services.Interfaces;
 using DAL.Context;
 using Data;
 using Microsoft.EntityFrameworkCore;
+using NLog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddSingleton<ILoggerManager, LoggerManager>();
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpContextAccessor();
+
 var app = builder.Build();
+
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
