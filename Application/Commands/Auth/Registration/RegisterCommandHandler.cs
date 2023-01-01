d3088506d95371c9085b04ef6c816e165e17125f
Andriy Marchyck <marchycandriy@gmail.com>
2023-01-01 18:48:40 +0200

    Fix

--- Application/Commands/Auth/Registration/RegisterCommandHandler.cs
+++ Application/Commands/Auth/Registration/RegisterCommandHandler.cs
@@ -3,25 +3,25 @@ using AutoMapper;
 using DAL.Repositry;
 using Data;
 using MediatR;
-using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
+using Application.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Commands.Auth.Registration
 {
     public class RegisterCommandHandler : IRequestHandler<RegisterUserCommand,(CustomerModel, string)>
     {
         private readonly IRepository<Customer> _repository;
+        private readonly IRepository<Cart> _cartRepository;
         private readonly IMapper _mapper;
-        private readonly SymmetricSecurityKey _key;
+        private readonly ITokenManager _tokenManager;
 
-        public RegisterCommandHandler(IRepository<Customer> repository,IMapper mapper)
+        public RegisterCommandHandler(IRepository<Customer> repository,IRepository<Cart> cartRepository,
+            IMapper mapper,ITokenManager tokenManager)
         {
             _repository = repository;
+            _cartRepository = cartRepository;
             _mapper = mapper;
-            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("adsf"));
+            _tokenManager = tokenManager;
         }
 
         public async Task<(CustomerModel, string)> Handle(RegisterUserCommand request,CancellationToken cancellationToken)
@@ -30,35 +30,28 @@ namespace Application.Commands.Auth.Registration
             {
                 throw new Exception("Passwords don't match");
             }
-
-            if (request.BirthDate == null)
+            if (await IsRegister(request.Login))
             {
-                request.BirthDate = DateOnly.MaxValue;
+                throw new ArgumentException("User with this id already registered!");
             }
+            request.BirthDate ??= DateOnly.Parse("2022-01-01");
             var passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
-
             request.Password = passwordHash;
 
             var customer = await _repository.AddAsync(_mapper.Map<Customer>(request));
             customer.Role = "Customer";
-            var claims = new List<Claim>
-            {
-                new Claim(JwtRegisteredClaimNames.Email, customer.Login),
-                new Claim(ClaimTypes.Role, customer.Role!)
-            };
-
-
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.Now.AddMinutes(60)
-            };
-
-            var tokenHandler = new JwtSecurityTokenHandler();
-
-            var token = tokenHandler.CreateToken(tokenDescriptor);
             await _repository.SaveChangesAsync();
-            return (_mapper.Map<CustomerModel>(customer), tokenHandler.WriteToken(token));
+
+            await _cartRepository.AddAsync(new Cart() { CustomerId = customer.Id });
+            await _cartRepository.SaveChangesAsync();
+            return (_mapper.Map<CustomerModel>(customer), _tokenManager.GenerateToken(customer));
+        }
+
+        private async Task<bool> IsRegister(string login)
+        {
+            var customer = await _repository.Query()
+                .FirstOrDefaultAsync(customer => customer.Login == login);
+            return customer != null;
         }
     }
 }
